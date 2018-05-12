29ecb1f05d7033dbd3d58ffe1181162738779f77
DinikaSen <dinikasen@gmail.com>
2018-05-12 11:58:28 +0530

    all files added

---
+++ test/dialign.test.js
@@ -0,0 +1,142 @@
+/*
+This tests require dialign2-2 binary executable to be installed in util/bin/dialign_package/src
+and DIALIGN2_DIR environment variable to be set pointing to util/bin/dialign_package/dialign2_dir
+
+To download dialign2-2 binary executable and build in the target location run following file :
+                    util/downloaderDialign.js
+
+NOTE : The environment variable paths are set by default unless user has changed them manually
+*/
+
+require('mocha');
+expect = require('chai').expect;
+should = require('chai').should();
+var stdout = require("test-console").stdout;
+var stderr = require("test-console").stderr;
+var resolve = require('path').resolve;
+const assert = require('assert');
+
+var restoreStdout;
+
+var dialign = require('../lib/dialign');
+
+
+describe('#Set custom execution path', function () {
+
+    it('customExecLocation should be set', function (done) {
+        var location = 'util/bin/dialign_package/src';
+        restoreStdout = stdout.ignore();
+        dialign.setCustomLocation(location);
+        restoreStdout();
+        dialign.customExecLocation.should.equal(resolve(location));
+        done();
+    });
+    it('CustomExecLocation should not be set', function (done) {
+        var location = 'lib';
+        restoreStdout = stdout.ignore();
+        dialign.setCustomLocation(location);
+        restoreStdout();
+        dialign.customExecLocation.should.not.equal(resolve(location));
+        done();
+    });
+});
+
+describe('#Set custom environment variable path', function () {
+
+    it('customEnvVarPath should be set', function (done) {
+        var location = 'util/bin/dialign_package/dialign2_dir';
+        restoreStdout = stdout.ignore();
+        dialign.setEnvironmentVar(location);
+        restoreStdout();
+        dialign.customEnvVarPath.should.equal(resolve(location));
+        done();
+    });
+    it('customEnvVarPath should not be set', function (done) {
+        var location = 'util';
+        restoreStdout = stdout.ignore();
+        dialign.setEnvironmentVar(location);
+        restoreStdout();
+        dialign.customEnvVarPath.should.not.equal(resolve(location));
+        done();
+    });
+});
+
+describe('#Align an unaligned sequence file', function () {
+    it('should execute dialign command (output format = fasta)', function (done) {
+        var inspect = stdout.inspect();
+        dialign.alignSeqFile('test/samples/example.fasta', 'fasta', function (err) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('Success.!');
+            }
+            inspect.restore();
+            assert.deepEqual(inspect.output[1], "Success.!\n");
+            done();
+        });
+    });
+
+    it('should execute dialign command (output format = clustalW)', function (done) {
+        var inspect = stdout.inspect();
+        dialign.alignSeqFile('test/samples/example.fasta', 'clustalW', function (err) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('Success.!');
+            }
+            inspect.restore();
+            assert.deepEqual(inspect.output[1], "Success.!\n");
+            done();
+        });
+    });
+});
+
+describe('#Align a string input of sequences', function () {
+    it('should execute dialign command (output format = fasta)', function (done) {
+        var inspect = stdout.inspect();
+        var input = '>test1\n' +
+            'ACDEFGHIKLMNPQRSTVWY\n' +
+            '>test2\n' +
+            'XXXXACDEFGHIMNXXXPQRSTVWY\n' +
+            '>test3\n' +
+            'ACDEFGHILMNXXXXXPQRSTVWYXXXX\n' +
+            '>test4\n' +
+            'XXXACDEFGHIKLMNPQRSTVWYXXX';
+        dialign.alignSeqString(input, 'fasta', function (err) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('Success.!');
+            }
+            inspect.restore();
+            assert.deepEqual(inspect.output[1], "Success.!\n");
+            done();
+        });
+    });
+    it('should execute dialign command (output format = clustalW)', function (done) {
+        var inspect = stdout.inspect();
+        var input = '>test1\n' +
+            'ACDEFGHIKLMNPQRSTVWY\n' +
+            '>test2\n' +
+            'XXXXACDEFGHIMNXXXPQRSTVWY\n' +
+            '>test3\n' +
+            'ACDEFGHILMNXXXXXPQRSTVWYXXXX\n' +
+            '>test4\n' +
+            'XXXACDEFGHIKLMNPQRSTVWYXXX';
+        dialign.alignSeqString(input, 'clustalW', function (err) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('Success.!');
+            }
+            inspect.restore();
+            assert.deepEqual(inspect.output[1], "Success.!\n");
+            done();
+        });
+    });
+});
+
---
+++ util/downloader.js
@@ -0,0 +1,74 @@
+const os = require('os');
+const download = require('download-file');
+const fs = require('fs');
+const path = require('path');
+var child_process = require('child_process');
+var targz = require('targz');
+
+var address = 'https://bibiserv.cebitec.uni-bielefeld.de/';
+var platform = os.platform();
+
+
+getDialignTool = function () {
+    if (platform == 'darwin') {
+        address += 'resources/download/dialign/dialign-2.2.1-universal-osx.dmg.zip';
+        downloadDaln(address,platform);
+    } else if (platform === 'linux') {
+        address += 'applications/dialign/resources/downloads/dialign-2.2.1-src.tar.gz';
+        downloadDaln(address,platform);
+    } else{
+        console.log('Sorry, jsBioTools cannot download Dialign for your operating system');
+    }
+}
+
+
+downloadDaln = function (url, platform) {
+    var src = './bin/';
+    if (platform === 'linux'){
+        src += 'dialign-2.2.1-src.tar.gz';
+    }else if (platform === 'darwin'){
+        src += 'dialign-2.2.1-universal-osx.dmg.zip';
+    }
+    console.log('Downloading Dialign from ', url);
+    download(url, {directory: './bin'}, function (err) {
+        if (err) {
+            console.log('Download failed');
+            console.log(err);
+        }
+        else {
+            console.log('Download complete');
+            targz.decompress({src: src, dest: './bin/'}, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Archive unzipped");
+                    if(platform === 'linux'){
+                        var execPath = path.resolve('./bin/dialign_package/src/');
+                        makeExecutable(execPath);
+                    } else if (platform === 'darwin'){
+                        // TODO : install dmg
+                    }
+                }
+            });
+        }
+    });
+}
+
+makeExecutable = function (location) {
+    child_process.exec('make', {cwd: location}, function (err) {
+        if (err) {
+            console.log('ERROR: ' + err);
+        } else {
+            child_process.exec('rm *.o', {cwd: location}, function (err) {
+                if (err) {
+                    console.log('ERROR: ' + err);
+                } else {
+                    console.log('binary executable created');
+                }
+            });
+        }
+    });
+}
+
+
+getDialignTool();
